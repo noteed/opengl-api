bb27932c487176c3e77e979a37661f904d7c2e16
Vo Minh Thu <noteed@gmail.com>
2010-07-30 11:54:21 +0200

    Cleaned the parser, added a printer and some sanity check.

--- Text/OpenGL/Spec.hs
+++ Text/OpenGL/Spec.hs
@@ -3,10 +3,20 @@
 -- Code to represent and parse the enumext.spec file of the OpenGL
 -- registry. It works on the revision: 11742 (dated Tue, 15 Jun 2010),
 -- i.e. OpenGL 4.1.
-module Text.OpenGL.Spec where
+--
+-- There is also some code to print the result back to something
+-- close to the original representation, for checking purpose.
+module Text.OpenGL.Spec (
+  EnumLine(..), StartEnum(..), Value(..), Extension(..),
+  enumLines, enumLine,
+  parseAndShow, reparse
+  ) where
 
+import Numeric (readHex, showHex)
+import Data.Char (toUpper)
 import Control.Applicative
-import Text.ParserCombinators.Parsec hiding (many, optional, (<|>))
+import Text.ParserCombinators.Parsec hiding
+  (many, optional, (<|>), token)
 
 ----------------------------------------------------------------------
 -- Data structures (line oriented)
@@ -15,7 +25,8 @@ import Text.ParserCombinators.Parsec hiding (many, optional, (<|>))
 -- Note : an interesting comment to recognize is Extension #xxx
 
 -- | A complete representation of an enum.spec or enumext.spec line.
--- Each variant maps to one line of text.
+-- Each variant maps to one line of text. See 'enumLines' to parse
+-- Strings to this representation.
 data EnumLine =
     Comment String
   -- ^ A comment on its own line, beginning with #.
@@ -27,24 +38,26 @@ data EnumLine =
   -- ^ A passthru line with its comment.
   | Enum String Value (Maybe String)
   -- ^ An enumerant, in format String = String # String.
-  | Use String String -- ^ A use line.
-  deriving Show
+  | Use String String
+  -- ^ A use line.
+  deriving (Eq, Show)
 
 -- | The different ways to start an enumeration.
 data StartEnum =
-    Version Int Int Bool -- (Maybe String)
+    Version Int Int Bool
   -- ^ Major, minor, the bool indicates if it is deprecated.
-  -- The Maybe String is in Start
   | Extension Extension String Bool
   -- ^ The extension prefix, its, and whether it is deprecated.
   | Name String
-  deriving Show
+  deriving (Eq, Show)
 
--- Hex 'xxx' optional trailing 'u' or 'ull'
-data Value = Hex String (Maybe String) | Deci String | String String
-  deriving Show
+data Value = Hex Integer (Maybe HexSuffix) | Deci Int | String String
+  deriving (Eq, Show)
 
--- Note: what for FfdMaskSGIX?
+data HexSuffix = U | Ull
+  deriving (Eq, Show)
+
+-- Note: what for FfdMaskSGIX? This will be a Name.
 -- | The different kinds of extension used to start an enumeration.
 data Extension =
   {-3-}DFX
@@ -72,28 +85,20 @@ data Extension =
   | SUN
   | SUNX
   | WIN
-  deriving (Read, Show)
+  deriving (Eq, Read, Show)
 
 ----------------------------------------------------------------------
 -- Parsing (line oriented)
 ----------------------------------------------------------------------
 
--- | Return the list of the unparsed lines, with their line number.
-enumLines :: [String] -> ([(Int,String)],[EnumLine])
-enumLines x = go 1 x [] []
-  where
-  go _ [] failure success = (reverse failure, reverse success)
-  go i (l:ls) failure success = case enumLine l of
-    Nothing -> go (i+1) ls ((i,l):failure) success
-    Just s -> go (i+1) ls failure (s:success)
+-- | Parse a complete enumext.spec.
+enumLines :: String -> Either ParseError [EnumLine]
+enumLines = parse (many pEnumLine <* eof) "enumLines"
 
-enumLine :: String -> Maybe EnumLine
-enumLine x = case enumLine' x of
-  Left _ -> Nothing
-  Right a -> Just a
-
-enumLine' :: String -> Either ParseError EnumLine
-enumLine' = parse pEnumLine "enumLine"
+-- | Try to parse a line to its 'EnumLine' representation.
+-- The '\n' character should be present at the end of the input.
+enumLine :: String -> Either ParseError EnumLine
+enumLine = parse pEnumLine "enumLine"
 
 type P a = GenParser Char () a
 
@@ -107,62 +112,64 @@ pEnumLine = choice
   , pUse
   ]
 
-blanks :: P ()
-blanks = () <$ many (oneOf " \t")
+blanks :: P String
+blanks = many (oneOf " \t")
 
-blanks1 :: P ()
-blanks1 = () <$ many1 (oneOf " \t")
+blanks1 :: P String
+blanks1 = many1 (oneOf " \t")
+
+token :: String -> P String
+token s = string s <* blanks
 
 eol :: P ()
-eol = () <$ char '\n' <|> eof
+eol = () <$ char '\n'
 
 digit' :: P Int
 digit' = (read . (:[])) <$> digit
 
--- TODO factor the xxxString (maybe take care of the trailing blanks),
--- maybe disallow beginning by a number.
+identifier :: P String
+identifier = many1 . oneOf $ "_" ++ ['0'..'9'] ++ ['a'..'z'] ++ ['A'..'Z']
 
-enumString :: P String
-enumString = (many1 . oneOf $ "_x" ++ ['0'..'9'] ++ ['A'..'Z']) <* blanks
+identifier_ :: P String
+identifier_ = identifier <* blanks
 
-useString :: P String
-useString = (many1 . oneOf $ "_" ++ ['0'..'9'] ++ ['a'..'z'] ++ ['A'..'Z']) <* blanks
-
-extString :: P String
-extString = (many1 . oneOf $ "_" ++ ['0'..'9'] ++ ['a'..'z'] ++ ['A'..'Z'])
-
--- TODO actual hexa and deci value
 value :: P Value
-value = Hex <$> try (string "0x" *> many1 hexDigit) <*>
-   (optional (try (string "ull") <|> string "u"))
-  <|> Deci <$> many1 digit
-  <|> String <$> extString
+value = Hex . fst . head . readHex <$>
+   try (string "0x" *> many1 hexDigit) <*>
+   hexSuffix
+  <|> Deci . read <$> many1 digit
+  <|> String <$> identifier
+
+hexSuffix :: P (Maybe HexSuffix)
+hexSuffix = optional $ try (Ull <$ string "ull") <|> (U <$ string "u")
 
 pComment :: P EnumLine
-pComment = Comment <$> (blanks *> char '#' *> many (noneOf "\n")) <* eol
+pComment = (\a b c -> Comment $ concat [a,b,c]) <$>
+  blanks <*> (string "#") <*> (many $ noneOf "\n")
+  <* eol
 
 pBlankLine :: P EnumLine
 pBlankLine = BlankLine <$ (blanks >> eol)
 
 pStart :: P EnumLine
 pStart = Start <$> pStartEnum <*>
-  (blanks *> string "enum:" *> blanks *> optional (many1 alphaNum)) <* eol
+  (blanks *> token "enum:" *> optional (many1 alphaNum)) <* eol
 
 pPassthru :: P EnumLine
 pPassthru = Passthru <$>
-  (string "passthru:" *> blanks *> string "/*"
-  *> blanks *> manyTill (noneOf "\n") (try $ string "*/")) <* eol
+  (token "passthru:" *> token "/*"
+  *> manyTill (noneOf "\n") (try $ string "*/")) <* eol
 
 pEnum :: P EnumLine
 pEnum = Enum <$>
-  (blanks1 *> enumString) <*>
+  (blanks1 *> identifier_) <*>
   (char '=' *> blanks *> value) <*>
   (optional $ blanks *> char '#' *> blanks *> many1 (noneOf "\n")) <* eol
 
 pUse :: P EnumLine
 pUse = Use <$>
-  (blanks1 *> string "use" *> blanks *> useString) <*>
-  (blanks *> enumString) <* eol
+  (blanks1 *> token "use" *> identifier_) <*>
+  identifier_ <* eol
 
 pStartEnum :: P StartEnum
 pStartEnum =
@@ -171,7 +178,7 @@ pStartEnum =
   (char '_' *> digit') <*>
   (maybe False (const True) <$> optional (string "_DEPRECATED"))
   <|>
-  Extension <$> pExt <*> (char '_' *> extString) <*>
+  Extension <$> pExt <*> (char '_' *> identifier) <*>
   (maybe False (const True) <$> optional (string "_DEPRECATED"))
   <|>
   Name <$> many alphaNum
@@ -206,4 +213,81 @@ pExt = choice $ map (fmap r . try . string)
   ]
   where r "3DFX" = {-3-}DFX
         r x = read x
+
+----------------------------------------------------------------------
+-- Printing
+-- This is mostly used as a sanity check by comparing the result against
+-- the original input string. Some spaces and tabs, and some zero-padding
+-- in hex numbers don't match. (The original format is aligned on
+-- 8-column-wide tabstops.)
+----------------------------------------------------------------------
+
+-- | This function is useful to check the parse result. It parse a
+-- enumext.spec file and try to print it back to the same format.
+parseAndShow :: FilePath -> IO ()
+parseAndShow fn = do
+  c <- readFile fn
+  case enumLines c of
+    Left err -> putStrLn $ "Parse error: " ++ show err
+    Right a -> putStrLn $ showEnumLines a
+
+showEnumLines :: [EnumLine] -> String
+showEnumLines = unlines . map showEnumLine
+
+showEnumLine :: EnumLine -> String
+showEnumLine el = case el of
+  Comment x -> x
+  BlankLine -> ""
+  Start se Nothing -> showStartEnum se ++ " enum:" 
+  Start se (Just x) -> showStartEnum se ++ " enum: " ++ x
+  Passthru x -> "passthru: /* " ++ x ++ "*/"
+  Enum a b Nothing -> "\t" ++ a ++ tabstop 55 a ++ "= " ++ showValue b
+  Enum a b (Just x) -> "\t" ++ a ++ tabstop 55 a ++ "= " ++ showValue b ++ " # " ++ x
+  Use a b -> "\tuse " ++ a ++ tabstop 39 (a ++ "    ") ++ "    " ++ b
+
+tabstop :: Int -> String -> String
+tabstop t a = replicate ((t - length a) `div` 8) '\t'
+
+showStartEnum :: StartEnum -> String
+showStartEnum se = case se of
+  Version i j True -> "VERSION_" ++ show i ++ "_" ++ show j ++ "_DEPRECATED"
+  Version i j False -> "VERSION_" ++ show i ++ "_" ++ show j
+  Extension e x True -> showExtension e ++ "_" ++ x ++ "_DEPRECATED"
+  Extension e x False -> showExtension e ++ "_" ++ x
+  Name x -> x
+
+showValue :: Value -> String
+showValue v = case v of
+  Hex i Nothing -> "0x" ++ showHex' i
+  Hex i (Just U) -> "0x" ++ showHex' i ++ "u"
+  Hex i (Just Ull) -> "0x" ++ showHex' i ++ "ull"
+  Deci i -> show i
+  String x -> x
+
+showHex' :: Integral a => a -> String
+showHex' i = replicate (4 - length h) '0' ++ h
+  where h = map toUpper (showHex i "")
+
+showExtension :: Extension -> String
+showExtension e = case e of
+  {-3-}DFX -> "3DFX"
+       _ -> show e
+
+----------------------------------------------------------------------
+-- Sanity check
+----------------------------------------------------------------------
+
+-- | Parse a file, and check the result can be parsed again to the same
+-- representation.
+reparse :: FilePath -> IO ()
+reparse fn = do
+  c <- readFile fn
+  case enumLines c of
+    Left err -> putStrLn $
+      "Error when parsing the original file: " ++ show err
+    Right a -> case enumLines $ showEnumLines a of
+      Left err -> putStrLn $
+        "Error when parsing the printed result: " ++ show err
+      Right b | a == b -> putStrLn "All's well that ends well."
+              | otherwise -> putStrLn "Ouch, not good."
 
